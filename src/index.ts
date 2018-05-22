bf47b2c7d7fbaf300fb8d83612036ef12f88c67e
Shady Khalifa <shekohex@gmail.com>
2018-05-22 19:11:06 +0200

    add root file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/*--------------------------------------------------------------
+ *  Copyright 2018 (c) Shady Khalifa (@shekohex).
+ *  Licensed under the MIT License.
+ *  All rights reserved.
+ *-------------------------------------------------------------*/
+
+// TODO: Build the Modules
