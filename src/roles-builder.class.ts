331f9f3e63ad497c113bd1ec0657c867201feb73
Shady Khalifa <shekohex@gmail.com>
2018-05-25 11:47:12 +0200

    feat(@core): add Roles builder class for crafting your roles

---
+++ src/roles-builder.class.ts
@@ -0,0 +1,7 @@
+import { AccessControl } from 'accesscontrol';
+/**
+ * @class RolesBuilder
+ * @extends AccessControl
+ * @description A helper class to build your roles
+ */
+export class RolesBuilder extends AccessControl {}
