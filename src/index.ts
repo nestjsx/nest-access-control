19f6e22673884c96555b7b7a1f013687dd1a112d
Shady Khalifa <shekohex@gmail.com>
2018-05-25 11:48:10 +0200

    the main file

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,8 @@
  *  All rights reserved.
  *-------------------------------------------------------------*/
 
-// TODO: Build the Modules
+export * from './decorators';
+export * from './access-control.module';
+export * from './roles-builder.class';
+export * from './role.interface';
+export * from './access-control.guard';
