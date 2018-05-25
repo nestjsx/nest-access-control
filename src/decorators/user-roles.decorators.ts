cb23c3a540886f5a8984a129a495bac9357882dc
Shady Khalifa <shekohex@gmail.com>
2018-05-25 13:21:49 +0200

    fix(@core): fix typo

--- src/decorators/user-roles.decorators.ts
+++ src/decorators/user-roles.decorators.ts
@@ -3,8 +3,8 @@ import { createParamDecorator, PipeTransform } from '@nestjs/common';
 /**
  * Access the user roles from the request object i.e `req.user.roles`.
  *
- * You can pass an optinal property key to the decorator to get it from the user object
- * e.g `@UserRoles('premissions')` will return the `req.user.premissions` insted.
+ * You can pass an optional property key to the decorator to get it from the user object
+ * e.g `@UserRoles('premissions')` will return the `req.user.premissions` instead.
  */
 export const UserRoles = createParamDecorator((data: string, req) => {
   return data ? req.user[data] : req.user.roles;
