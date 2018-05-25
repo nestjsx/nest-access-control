54a43c2442c656366ee8c880ba22bba365ca9b9f
Shady Khalifa <shekohex@gmail.com>
2018-05-25 13:21:25 +0200

    example: update examples

--- example/src/app.service.ts
+++ example/src/app.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRolesBuilder, RolesBuilder } from '../../src';
 @Injectable()
 export class AppService {
-  constructor(
-  ) {}
+  private videos: string[] = ['Funny Cats', 'Ninja Dogs'];
   root(roles: string[]) {
     return {
+      videos: this.videos,
       userRoles: roles
     }
   }
