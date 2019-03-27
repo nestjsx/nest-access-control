c0b843f29445fec64a10353cd3e5e1e38a883be1
Daniel Friesen <daniel@nadir-seen-fire.com>
2019-03-26 18:17:07 -0700

    Export ROLES_BUILDER_TOKEN
    This allows RoleBuilder to be injected into factory providers without referencing the constants file.

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,4 @@ export * from './access-control.module';
 export * from './roles-builder.class';
 export * from './role.interface';
 export * from './access-control.guard';
+export { ROLES_BUILDER_TOKEN } from './constants'
