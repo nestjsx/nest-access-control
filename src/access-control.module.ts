6325d921e03c82f77f645129d6f4857fb5910ace
rjlopezdev <rjlopezmad@gmail.com>
2019-09-12 23:43:10 +0200

    feat: add GrantsController

--- src/access-control.module.ts
+++ src/access-control.module.ts
@@ -1,6 +1,8 @@
 import { Module, DynamicModule, Global, Abstract, Type } from '@nestjs/common';
+import { PATH_METADATA } from '@nestjs/common/constants';
 import { RolesBuilder } from './roles-builder.class';
 import { ROLES_BUILDER_TOKEN } from './constants';
+import { GrantsController } from './grants.controller';
 
 @Global()
 @Module({})
@@ -8,11 +10,18 @@ export class AccessControlModule {
   /**
    * Register a pre-defined roles
    * @param {RolesBuilder} roles  A list containing the access grant
+   * @param {string} serveGrants A name of endpoint which serves grants
    * definitions. See the structure of this object in the examples.
    */
-  public static forRoles(roles: RolesBuilder): DynamicModule {
+  public static forRoles(roles: RolesBuilder, serveGrants?: string): DynamicModule {
+
+    Reflect.defineMetadata(PATH_METADATA, serveGrants, GrantsController);
+
     return {
       module: AccessControlModule,
+      controllers: [
+        ...serveGrants ? [GrantsController] : [],
+      ],
       providers: [
         {
           provide: ROLES_BUILDER_TOKEN,
