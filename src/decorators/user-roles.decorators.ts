4b8ffe7921286f33101ad7c52ab62527e50f0a36
tupe12334 <ofek.gabay.he@gmail.com>
2021-12-09 00:54:44 +0200

    feat(decorator): add support for graphql in user roles decorator

--- src/decorators/user-roles.decorators.ts
+++ src/decorators/user-roles.decorators.ts
@@ -1,4 +1,26 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
+import { Role } from '..';
+
+function userFactory<T>(ctx: ExecutionContext): T {
+  const contextType = ctx.getType();
+  if (contextType === 'http') {
+    // do something that is only important in the context of regular HTTP requests (REST)
+    const { user } = ctx.switchToHttp().getRequest();
+    return user;
+  } else if (contextType === 'rpc') {
+    // do something that is only important in the context of Microservice requests
+    throw new Error('Rpc context is not implemented yet');
+  } else if (contextType === 'ws') {
+    // do something that is only important in the context of Websockets requests
+    throw new Error('Websockets context is not implemented yet');
+  } else if (ctx.getType<GqlContextType>() === 'graphql') {
+    // do something that is only important in the context of GraphQL requests
+    const gqlExecutionContext = GqlExecutionContext.create(ctx);
+    return gqlExecutionContext.getContext().req.user;
+  }
+  throw new Error('Invalid context');
+}
 
 /**
  * Access the user roles from the request object i.e `req.user.roles`.
@@ -6,8 +28,9 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
  * You can pass an optional property key to the decorator to get it from the user object
  * e.g `@UserRoles('permissions')` will return the `req.user.permissions` instead.
  */
-export const UserRoles = createParamDecorator((data: string,  ctx: ExecutionContext) => {
-
-  const request = ctx.switchToHttp().getRequest();
-  return data ? request.user[data] : request.user.roles;
-});
+export const UserRoles = createParamDecorator<string, ExecutionContext, (Role | string)[]>(
+  (data: string, ctx: ExecutionContext) => {
+    const user = userFactory<any>(ctx);
+    return data ? user[data] : user.roles;
+  },
+);
