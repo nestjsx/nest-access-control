3cce0b01e72541af1af5ba81c40a19171c931575
Shady Khalifa <shekohex@gmail.com>
2022-02-20 16:46:32 +0200

    Publish v2.0.4

--- lib/decorators/user-roles.decorators.d.ts
+++ lib/decorators/user-roles.decorators.d.ts
@@ -3,5 +3,6 @@
  *
  * You can pass an optional property key to the decorator to get it from the user object
  * e.g `@UserRoles('permissions')` will return the `req.user.permissions` instead.
+ * In case that the request is missing User object the function will return null
  */
 export declare const UserRoles: (...dataOrPipes: (string | import("@nestjs/common").PipeTransform<any, any> | import("@nestjs/common").Type<import("@nestjs/common").PipeTransform<any, any>>)[]) => ParameterDecorator;
