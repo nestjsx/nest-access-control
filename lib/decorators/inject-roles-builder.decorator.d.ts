3c2dbd0ce88b30829967a8300da02afdf69e63b6
shekohex <dev+github@shadykhalifa.me>
2023-10-11 15:36:04 +0000

    Publish v3.1.0

--- lib/decorators/inject-roles-builder.decorator.d.ts
+++ lib/decorators/inject-roles-builder.decorator.d.ts
@@ -1,4 +1,4 @@
 /**
  *  Get access to the underlying `RolesBuilder` Object
  */
-export declare const InjectRolesBuilder: () => (target: object, key: string | symbol, index?: number) => void;
+export declare const InjectRolesBuilder: () => PropertyDecorator & ParameterDecorator;
